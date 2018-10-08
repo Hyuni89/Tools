381f2ee44d9ad1aaf9796ff0d0f9b359d3fb5895
Hyuni89 <cuhcuh1@naver.com>
2018-10-08 14:18:56 +0900

    update

--- DevelopmentSetting/run.sh
+++ DevelopmentSetting/run.sh
@@ -17,3 +17,22 @@ else
 	exit
 fi
 
+# sudo apt install zsh
+# brew install zsh zsh-completions
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# zsh plugin
+# zsh-syntax-highlighting
+# git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+# zsh-autosuggestions
+# git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+# add ~/.zshrc
+# plugins=(
+#   git
+#   zsh-syntax-highlighting
+#   zsh-autosuggestions
+# )
+
+# SpaceVim
+# curl -sLf https://spacevim.org/install.sh | bash
+
